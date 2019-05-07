b5f4b5c6c3deca4ca25969d292dc5411b9660363
ruihildt <ruihildt@protonmail.com>
2019-05-07 19:45:48 +0200

    finish up to challenge 4 of array-methods.js

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -69,15 +69,8 @@ console.log(fullName);
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 
 // Create an array with all firstnames
-let firstNames = [];
 
-runners.forEach(element => {
-    let firstname = `${element.first_name}`;
-    firstNames.push(firstname);
-});
-
-// Use map to change every firstnames to uppercase
-const allCaps = firstNames.map(element => element.toUpperCase());
+const allCaps = runners.map(runner => (runner.first_name.toUpperCase()));
 
 console.log(allCaps);
 
@@ -86,15 +79,21 @@ console.log(allCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+const largeShirts = runners.filter(runner => runner.shirt_size == "L");
+
 console.log(largeShirts);
 
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
-console.log(ticketPriceTotal);
 
+let reducer = function(accumulator, item) {
+    return accumulator + item.donation;
+}
+
+const ticketPriceTotal = runners.reduce(reducer, 0)
+
+console.log(ticketPriceTotal); 
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
